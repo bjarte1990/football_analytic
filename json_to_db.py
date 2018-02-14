f8dc745c956884a3b3d7a677afd84bb1d9511d83
piglercs <piglercs@starschema.net>
2018-02-14 08:50:17 +0100

    game info parsed

--- json_to_db.py
+++ json_to_db.py
@@ -8,13 +8,15 @@ filename = '2017_regular_season.json'
 with open(filename, 'r') as handle:
     parsed = json.load(handle)
 
-lines = defaultdict()
+lines = pd.DataFrame()
 for game_week in parsed:
     year = game_week['year']
     week = game_week['week']
     games = game_week['matches']
 
     for game in games:
+        print(game.keys())
+        game_line_dict = {}
         away_team_coach = game['away_team_coach']
         away_team = game['away_team']
         away_team_score = game['away_team_score']
@@ -22,7 +24,7 @@ for game_week in parsed:
         home_team = game['home_team']
         home_team_score = game['home_team_score']
         match_date = game['match_date']
-        attendance = game['attendance']
+        attendance = game['attendance'].replace(',', '')
         roof = game['roof']
         over_under = game['over_under']
         try:
@@ -40,24 +42,32 @@ for game_week in parsed:
         home_team_short = ''.join(map(lambda x: x[:2], home_team.split(' ')))
         match_id = str(year) + str(week) + away_team_short.upper() + home_team_short.upper()
 
-        lines.setdefault('match_id', []).append(match_id)
-        lines.setdefault('away_team', []).append(away_team)
-        lines.setdefault('home_team', []).append(home_team)
-        lines.setdefault('away_team_coach', []).append(away_team_coach)
-        lines.setdefault('home_team_coach', []).append(home_team_coach)
-        lines.setdefault('away_team_score', []).append(away_team_score)
-        lines.setdefault('home_team_score', []).append(home_team_score)
-        lines.setdefault('match_date', []).append(match_date)
-        lines.setdefault('attendance', []).append(attendance)
-        lines.setdefault('roof', []).append(roof)
-        lines.setdefault('over_under', []).append(over_under)
-        lines.setdefault('weather', []).append(weather)
-        lines.setdefault('length_of_game', []).append(length_of_game)
-        lines.setdefault('surface', []).append(surface)
-        lines.setdefault('stadium', []).append(stadium)
-        lines.setdefault('vegas_line', []).append(vegas_line)
-        lines.setdefault('match_start_time', []).append(match_start_time)
-        lines.setdefault('won_toss', []).append(won_toss)
+        game_line_dict['match_id'] = match_id
+        game_line_dict['away_team'] = away_team
+        game_line_dict['home_team'] = home_team
+        game_line_dict['away_team_coach'] = away_team_coach
+        game_line_dict['home_team_coach'] = home_team_coach
+        game_line_dict['away_team_score'] = away_team_score
+        game_line_dict['home_team_score'] = home_team_score
+        game_line_dict['match_date'] = match_date
+        game_line_dict['attendance'] = attendance
+        game_line_dict['roof'] = roof
+        game_line_dict['over_under'] = over_under
+        game_line_dict['weather'] = weather
+        game_line_dict['length_of_game'] = length_of_game
+        game_line_dict['surface'] = surface
+        game_line_dict['stadium'] = stadium
+        game_line_dict['vegas_line'] = vegas_line
+        game_line_dict['match_start_time'] = match_start_time
+        game_line_dict['won_toss'] = won_toss
+        s = pd.Series(game_line_dict)
+        lines = lines.append(s, ignore_index=True)
 
-data = pd.DataFrame(lines)
-data.to_csv('2017.csv', index=False)
+
+# print(lines)
+# data = pd.DataFrame(lines)
+# data.to_csv('game_info.2017', index=False, columns=['match_id', 'away_team', 'home_team', 'away_team_coach',
+#                                               'home_team_coach', 'away_team_score', 'home_team_score',
+#                                               'match_date', 'attendance', 'roof', 'weather', 'surface',
+#                                               'stadium', 'match_start_time', 'length_of_game', 'won_toss',
+#                                               'over_under', 'vegas_line'], header=False)
